7aa0604a22bc3579e345cfe60ec2e2503964e9ed
g3n35i5 <janfschmidt@me.com>
2019-08-10 21:53:57 +0200

    Linter

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-
 var debug = true;
 
 main = new Vue({
-	el: "#main",
-	data: {
-		step: 1,
-		currentResource: null
-	},
-	methods: {
-		home() {
-			if (Object.keys(cart.courses).length > 0) {
-				if (!confirm('Are you sure, that you want to clear your cart?')) {
-					return 0;
-				}
-			}
+    el: "#main",
+    data: {
+        step: 1,
+        currentResource: null
+    },
+    methods: {
 
-			cart.clearCart();
-			this.currentResource = null;
-			if (this.step !== 1) {
-				this.step = 1;
-			} else {
-				this.$refs.fileList.listDir('/');
-			}
-		}
-	}
-})
+    	showResource(dir) {
+    		return showResource(dir);
+		},
+        home() {
+            if (Object.keys(cart.courses).length > 0) {
+                if (!confirm('Are you sure, that you want to clear your cart?')) {
+                    return 0;
+                }
+            }
+
+            cart.clearCart();
+            this.currentResource = null;
+            if (this.step !== 1) {
+                this.step = 1;
+            } else {
+                this.$refs.fileList.listDir('/');
+            }
+        }
+    }
+});
