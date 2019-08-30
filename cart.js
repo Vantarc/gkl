315a52c5ce1855c7219967d65aabc578eeef5d07
g3n35i5 <janfschmidt@me.com>
2019-08-30 16:55:50 +0200

    Forcing the price to be displayed with two digits after separator

--- cart.js
+++ cart.js
@@ -269,7 +269,7 @@ Vue.filter('toCurrency', function (value) {
 	const formatter = new Intl.NumberFormat('de-DE', {
 		style: 'currency',
 		currency: 'EUR',
-		minimumFractionDigits: 0
+		minimumFractionDigits: 2
 	});
 	return formatter.format(value);
 });
