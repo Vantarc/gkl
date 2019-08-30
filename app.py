a8c225709ea04e7f5e1df1cc9ca30706ccff5053
g3n35i5 <janfschmidt@me.com>
2019-08-31 01:11:01 +0200

    Adding configuration

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template
+from configuration import Configuration
 
 app = Flask(__name__)
+app.config.from_object(Configuration)
 
 
 @app.route('/')
---
+++ configuration.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'g3n35i5'
+
+
+class Configuration:
+    HOST = '127.0.0.1'
+    PORT = 8000
